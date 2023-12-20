10d51e0210aa54147f769ac28377a848f1f072f2
Sreypich Khoeun <khoeunsreypich.kh@gmail.com>
2023-12-20 14:49:52 +0700

    do stack

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -82,8 +82,11 @@ class myHome extends StatelessWidget {
                       SizedBox(
                           width: 150,
                           height: 150,
-                          child: Image.network(
-                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQhxVn-YlbDCRfY7sZgkuFkq8TfQpW6-36QA&usqp=CAU')),
+                          child: Align(
+                            alignment: Alignment.bottomRight,
+                            child: Image.network(
+                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQhxVn-YlbDCRfY7sZgkuFkq8TfQpW6-36QA&usqp=CAU'),
+                          )),
                     ],
                   ),
                 ],
@@ -122,8 +125,11 @@ class myHome extends StatelessWidget {
                                 SizedBox(
                                     width: 150,
                                     height: 150,
-                                    child: Image.network(
-                                        'https://images.deliveryhero.io/image/foodpanda/homepage/refresh-hero-home-kh.png')),
+                                    child: Align(
+                                      alignment: Alignment.bottomRight,
+                                      child: Image.network(
+                                          'https://images.deliveryhero.io/image/foodpanda/homepage/refresh-hero-home-kh.png'),
+                                    )),
                               ],
                             ),
                           ],
@@ -134,6 +140,7 @@ class myHome extends StatelessWidget {
                   Expanded(
                     child: Container(
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
                           Expanded(
                             flex: 4,
@@ -157,8 +164,14 @@ class myHome extends StatelessWidget {
                                         SizedBox(
                                           width: 160,
                                           height: 120,
-                                          child: Image.network(
-                                              'https://images.deliveryhero.io/image/foodpanda/homepage/refresh-hero-home-kh.png'),
+                                          child: Align(
+                                            alignment: Alignment.bottomRight,
+                                            child: Container(
+                                              margin: EdgeInsets.only(left: 10),
+                                              child: Image.network(
+                                                  'https://images.deliveryhero.io/image/foodpanda/homepage/refresh-hero-home-kh.png'),
+                                            ),
+                                          ),
                                         )
                                       ],
                                     )
@@ -169,40 +182,52 @@ class myHome extends StatelessWidget {
                           ),
                           Expanded(
                             flex: 2,
-                            child: Container(
-                              decoration: boxDecoration,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 5, top: 8),
+                              child: Container(
+                                decoration: boxDecoration,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
                                   children: [
-                                    Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            'Pandasend',
-                                            style: TextStyle(
-                                                fontSize: 25,
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                          Text('Express'),
-                                          Text('Delivery'),
-                                          Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Column(
-                                              children: [
-                                                SizedBox(
-                                                  width: 30,
-                                                  height: 30,
-                                                  child: Image.network(
-                                                      'https://images.deliveryhero.io/image/foodpanda/homepage/refresh-hero-home-kh.png'),
-                                                ),
-                                              ],
-                                            ),
-                                          )
-                                        ]),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              const Text(
+                                                'Pandasend',
+                                                style: TextStyle(
+                                                    fontSize: 25,
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                              const Text('Express'),
+                                              //const Text('Delivery'),
+                                              Row(
+                                                children: [
+                                                  const Text('Delivery'),
+                                                  Column(
+                                                    children: [
+                                                      SizedBox(
+                                                        width: 125,
+                                                        height: 35.5,
+                                                        child: Align(
+                                                          alignment: Alignment
+                                                              .bottomRight,
+                                                          child: Image.network(
+                                                              'https://images.deliveryhero.io/image/foodpanda/homepage/refresh-hero-home-kh.png'),
+                                                        ),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ],
+                                              )
+                                            ]),
+                                      ],
+                                    ),
                                   ],
                                 ),
                               ),
@@ -216,7 +241,99 @@ class myHome extends StatelessWidget {
               ),
             ),
           ),
-        )
+        ),
+        const SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.only(left: 12, top: 10),
+            child: Text(
+              'Popular Restaurants',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        // SliverToBoxAdapter(
+        //   child: Stack(
+        //     children: [
+        //       Padding(
+        //         padding: const EdgeInsets.only(left: 12, top: 12),
+        //         child: SizedBox(
+        //           height: 200,
+        //           child: Container(
+        //             decoration: BoxDecoration(
+        //                 borderRadius: BorderRadius.circular(20)),
+        //             child: ClipRRect(
+        //               borderRadius: BorderRadius.circular(15),
+        //               child: Image.network(
+        //                   'https://d1ralsognjng37.cloudfront.net/a96aeaae-f23e-4ae4-b885-a41ad288c0ea.jpeg'),
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // )
+        SliverToBoxAdapter(
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(12),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: SizedBox(
+                              height: 250,
+                              child: Image.network(
+                                  'https://d1ralsognjng37.cloudfront.net/a96aeaae-f23e-4ae4-b885-a41ad288c0ea.jpeg'),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Positioned.fill(
+                        child: Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(bottom: 200, left: 12),
+                            child: Text(
+                              '20% OFF ',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white,
+                                  backgroundColor: Colors.pink),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Positioned.fill(
+                        child: Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(bottom: 225, left: 12),
+                            child: Text(
+                              'Free Delivery ',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white,
+                                  backgroundColor: Colors.pink),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ],
     );
   }
