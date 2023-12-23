60aa6680c29b792bae4707750c387180e4e325f7
Sreypov Or <orsreypov.kh@gmail.com>
2023-12-23 13:14:27 +0700

    card

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widget/card_food_drink.dart';
+
 class myHome extends StatelessWidget {
   myHome({
     super.key,
@@ -285,65 +287,19 @@ class myHome extends StatelessWidget {
         //   ),
         // )
         SliverToBoxAdapter(
-          child: Stack(
-            children: [
-              Column(
-                children: [
-                  Stack(
-                    children: [
-                      Container(
-                        child: Padding(
-                          padding: const EdgeInsets.all(12),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(15),
-                            child: SizedBox(
-                              height: 250,
-                              child: Image.network(
-                                  'https://d1ralsognjng37.cloudfront.net/a96aeaae-f23e-4ae4-b885-a41ad288c0ea.jpeg'),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const Positioned.fill(
-                        child: Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Padding(
-                            padding: EdgeInsets.only(bottom: 200, left: 12),
-                            child: Text(
-                              '20% OFF ',
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white,
-                                  backgroundColor: Colors.pink),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const Positioned.fill(
-                        child: Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Padding(
-                            padding: EdgeInsets.only(bottom: 225, left: 12),
-                            child: Text(
-                              'Free Delivery ',
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white,
-                                  backgroundColor: Colors.pink),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              )
-            ],
+          child: Container(
+            height: 300,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return CardFoodDrink();
+                },
+                ),
           ),
         ),
       ],
     );
   }
 }
+
